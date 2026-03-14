50439bd7086f9d9cf664665f3116cca1d23a49b3
roma919111 <maq9801@gmail.com>
2026-03-14 08:53:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-import express from "express"
-import cors from "cors"
-import axios from "axios"
+const express = require("express")
+const cors = require("cors")
+const axios = require("axios")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-// API KEY الخاص بك
 const API_KEY = process.env.ARK_API_KEY
 
 
-// تخزين أكواد الرصيد
+// أكواد الرصيد
 
 const codes = {
 
@@ -39,7 +38,7 @@ method:"POST",
 url:"https://ark.ap-southeast.bytepluses.com/api/v3/images/generations",
 
 headers:{
-"Authorization":`Bearer ${API_KEY}`,
+"Authorization":"Bearer "+API_KEY,
 "Content-Type":"application/json"
 },
 
@@ -66,7 +65,7 @@ error:"generation_failed"
 })
 
 
-// تفعيل كود الرصيد
+// تفعيل الكود
 
 app.post("/activate-code",(req,res)=>{
 
@@ -85,14 +84,14 @@ if(codes[code].used){
 
 return res.json({
 success:false,
-message:"تم استخدام الكود سابقاً"
+message:"تم استخدام الكود سابقا"
 })
 
 }
 
 codes[code].used = true
 
-return res.json({
+res.json({
 success:true,
 credits:codes[code].credits
 })
@@ -100,12 +99,19 @@ credits:codes[code].credits
 })
 
 
-// تشغيل السيرفر
+// اختبار السيرفر
+
+app.get("/",(req,res)=>{
+
+res.send("AI server working")
+
+})
+
 
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT,()=>{
 
-console.log("AI server working on port",PORT)
+console.log("AI server working on port "+PORT)
 
 })
