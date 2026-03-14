5c7fb9a9400698ea37484effb224322f1d3b2309
roma919111 <maq9801@gmail.com>
2026-03-14 09:49:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express"
 import cors from "cors"
-import fetch from "node-fetch"
 
 const app = express()
 
@@ -8,63 +7,58 @@ app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
-
-// استخدام اسم المتغير الموجود في Railway
 const API_KEY = process.env.API_KEY
 
-
-app.get("/", (req, res) => {
-  res.send("AI server working")
+app.get("/", (req,res)=>{
+res.send("AI server working")
 })
 
+app.post("/generate", async (req,res)=>{
 
-app.post("/generate", async (req, res) => {
+try{
 
-  try {
+const { prompt } = req.body
 
-    const { prompt } = req.body
+const response = await fetch(
+"https://ark.ap-southeast.bytepluses.com/api/v3/images/generations",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json",
+"Authorization":`Bearer ${API_KEY}`
+},
+body:JSON.stringify({
+model:"ep-20260227140001-vlp9z",
+prompt:prompt,
+sequential_image_generation:"disabled",
+response_format:"url",
+size:"2K",
+stream:false,
+watermark:true
+})
+}
+)
 
-    const response = await fetch(
-      "https://ark.ap-southeast.bytepluses.com/api/v3/images/generations",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-          model: "ep-20260227140001-vlp9z",
-          prompt: prompt,
-          sequential_image_generation: "disabled",
-          response_format: "url",
-          size: "2K",
-          stream: false,
-          watermark: true
-        })
-      }
-    )
+const data = await response.json()
 
-    const data = await response.json()
+const image = data?.data?.[0]?.url
 
-    const image = data.data?.[0]?.url
+res.json({
+image:image
+})
 
-    res.json({
-      image: image
-    })
+}catch(error){
 
-  } catch (error) {
+console.log(error)
 
-    console.log(error)
+res.status(500).json({
+error:"generation failed"
+})
 
-    res.status(500).json({
-      error: "generation failed"
-    })
-
-  }
+}
 
 })
 
-
-app.listen(PORT, () => {
-  console.log("Server running")
+app.listen(PORT,()=>{
+console.log("server running")
 })
